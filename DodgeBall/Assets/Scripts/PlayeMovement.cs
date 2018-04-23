c8456dd1e1f67c2e9d8c6c7e1b4e1a51ba8ee077
jaxsonwang <1355542655@qq.com>
2018-04-23 09:20:13 +0800

    添加发射球，玩家左右移动，球与玩家碰撞检测

---
+++ DodgeBall/Assets/Scripts/PlayeMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeMovement : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        transform.Translate(h, 0, 0);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("发生碰撞:" + other.gameObject.name);
+    }
+}
---
+++ DodgeBall/Assets/Scripts/ShootingManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingManager : MonoBehaviour {
+
+    public GameObject shootingPosition;
+    public Rigidbody ball;
+    public float power = 600f;
+    public Vector3 towards = new Vector3(0, 0.1f, 1);
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("shootingBalls", 2f, 1f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (Input.GetButtonUp("Fire1")) {
+        //    Rigidbody instance = Instantiate(ball, transform.position, transform.rotation) as Rigidbody;
+
+        //    Vector3 fwd = transform.TransformDirection(towards);
+           
+        //    instance.AddForce(fwd * power);
+
+        //}
+	}
+
+    void shootingBalls() {
+        Rigidbody instance = Instantiate(ball, transform.position, transform.rotation) as Rigidbody;
+
+        Vector3 fwd = transform.TransformDirection(towards);
+
+        instance.AddForce(fwd * power);
+    }
+}
