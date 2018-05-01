c9ae440d7f4714c80a03b282a5f5ff52ea7d7434
jaxsonwang <1355542655@qq.com>
2018-05-01 20:15:08 +0800

    增加主菜单

--- DodgeBall/Assets/Scripts/ShootingManager.cs
+++ DodgeBall/Assets/Scripts/ShootingManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,32 +8,108 @@ public class ShootingManager : MonoBehaviour {
     public GameObject shootingPosition;
     public Rigidbody ball;
     public float power = 600f;
-    public Vector3 towards = new Vector3(0, 0.1f, 1);
+    
+    public float shootMoveSpeed = 0.4f;
+    public Vector3 moveDirection = Vector3.left;
     // Use this for initialization
-    void Start () {
+    //void Awake()
+    //{
+    //    positions = new Transform[movePosition.childCount];
+    //    for (int i = 0; i < 2; i++)
+    //    {
+    //        positions[i] = movePosition.GetChild(i);
+    //    }
 
-        InvokeRepeating("shootingBalls", 2f, 1f);
+    //}
+
+    void Start () {
+        //positions = movePoints.positions;
+        InvokeRepeating("shootBalls", 2f, 1f);
+        //InvokeRepeating("moveShootPosition",2f,1f);
 
     }
-	
+
 	// Update is called once per frame
 	void Update () {
-
+        
         //if (Input.GetButtonUp("Fire1")) {
         //    Rigidbody instance = Instantiate(ball, transform.position, transform.rotation) as Rigidbody;
 
         //    Vector3 fwd = transform.TransformDirection(towards);
-           
+
         //    instance.AddForce(fwd * power);
 
         //}
+        
+        moveShootPosition();
 	}
 
-    void shootingBalls() {
+    //发射球
+    void shootBalls() {
+        System.Random rd = new System.Random();
+
+        Vector3 towards = new Vector3(0, rd.Next(0, 10) * 0.1f * 0.3f, 1);
+
         Rigidbody instance = Instantiate(ball, transform.position, transform.rotation) as Rigidbody;
 
         Vector3 fwd = transform.TransformDirection(towards);
-
+        
         instance.AddForce(fwd * power);
+
+        Destroy(instance.gameObject,3f);
+    }
+
+    //移动发射球的坐标
+    void moveShootPosition() {
+        //Vector3 c = moveBounds.center;
+        //Vector3 s = moveBounds.size;
+
+        //float x = Random.Range(c.x - s.x * 0.5f, c.x - s.x * 0.5f);
+        //float y = Random.Range(c.y - s.y * 0.5f, c.y - s.y * 0.5f);
+        //float z = Random.Range(c.z - s.z * 0.5f, c.z - s.z * 0.5f);
+
+
+        //Vector3 point = new Vector3(x, y, z);
+
+        // transform.Translate(point);
+
+        //float letfPosX = -5f;
+        //float rightPosX = -2f;
+
+        //if (transform.position.x >= letfPosX)
+        //{
+        //    float moveLeft = 1 * Time.deltaTime* shootMoveSpeed;
+        //    transform.Translate(moveLeft, 0, 0);
+        //}
+        //else if ( transform.position.x < rightPosX)
+        //{
+        //    float moveRight = -1 * Time.deltaTime * shootMoveSpeed;
+        //    transform.Translate(moveRight, 0, 0);
+        //}
+
+        //if (index > positions.Length - 1) return;
+        //transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * shootMoveSpeed);
+        //Debug.Log("positions[index].position:"+positions[index].position);
+        //Debug.Log("transform.position:" + transform.position);
+
+        //if (Vector3.Distance(positions[index].position, transform.position) < 0.5f)
+        //{
+        //    index++;
+        //    index = index >= 1 ? 0 : 1;
+        //    Debug.Log(index);
+
+        //}
+        //if (index > positions.Length - 1)
+        //{
+        //    ReachDestination();
+        //}
+        if (moveDirection == Vector3.left && transform.position.x > -2.45f || moveDirection == Vector3.right && transform.position.x < -5.56f)
+        {
+            moveDirection = -moveDirection;
+        }
+        transform.Translate( moveDirection * Time.deltaTime * shootMoveSpeed);
+
+        
+
     }
 }
---
+++ DodgeBall/Assets/Scripts/movePoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePoints : MonoBehaviour {
+
+    // Use this for initialization
+
+    public static Transform[] positions;
+
+    // Use this for initialization
+    void Awake()
+    {
+        positions = new Transform[transform.childCount];
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = transform.GetChild(i);
+        }
+
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
