d5ee13e051493fd09b9c1b6237cdf256486a197f
jaxsonwang <1355542655@qq.com>
2018-05-03 22:33:32 +0800

    增加游戏关卡选择，增加游戏场景

--- DodgeBall/Assets/Scripts/GameManager.cs
+++ DodgeBall/Assets/Scripts/GameManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject titleText;
+    public GameObject startGameText;
+
+    public GameObject levelGroup1;
+    public GameObject levelGroup2;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -16,6 +21,26 @@ public class GameManager : MonoBehaviour {
 
     public void OnStartGame()
     {
+        titleText.GetComponent<Animation>().Play("Title_Move_Out");
+        startGameText.GetComponent<Animation>().Play("StartButton_Move_Out");
+        Invoke("showLevel",1.0f);
+      
+    }
+
+    public void showLevel()
+    {
+        // Application.LoadLevel("MainScene");
+        levelGroup1.GetComponent<Animation>().Play("Level_Move_Down");
+        levelGroup2.GetComponent<Animation>().Play("Level_Move_Up");
+    }
+
+    public void loadLevel1()
+    {
         Application.LoadLevel("MainScene");
+    }
+
+    public void loadLevel2()
+    {
+        Application.LoadLevel("Level1");
     }
 }
