44665d5ab71e262d9686c2fb34f8f2c89d6b62f4
jaxsonwang <1355542655@qq.com>
2018-05-04 23:58:34 +0800

    改变鼠标UI触发方式

---
+++ DodgeBall/Assets/Scripts/MouseControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.UI;
+public class MouseControl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject obj;
+    private bool _isEnter;
+    private float _timer;
+
+
+    // public String methodName;
+
+    public GameObject gameManager;
+    public GameObject buttonComponent;
+    public int methodID;
+
+    void Update()
+    {
+
+        _timer += Time.deltaTime;
+
+        //Debug.Log("触摸计时");
+        if (_isEnter && _timer - 2.0f > 0f)
+        {
+          //  obj.SetActive(true);
+            Debug.Log("触摸开始");
+
+          //  gameManager.GetComponent<GameManager>().OnStartGame();
+            buttonComponent.GetComponent<SceneManager>().SendMessage("methodSelectWithID", methodID);
+
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _timer = 0;
+        _isEnter = true;
+        Debug.Log("触摸进入");
+
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isEnter = false;
+       // obj.SetActive(false);
+        Debug.Log("触摸退出");
+    }
+}
---
+++ DodgeBall/Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public GameObject hand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void jadge() {
+
+        hand.GetComponent<Transform>().position.z  
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
